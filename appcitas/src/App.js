5898e21c3f50addee50c3c4c36d42f77d745aa2f
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2021-03-28 18:39:28 -0300

    Creo componente Cita para mostar las citas

--- appcitas/src/App.js
+++ appcitas/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
 
@@ -25,7 +26,13 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            2
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita
+                key={cita.id}
+                cita={cita}
+              />
+            ))}
           </div>
 
         </div>
