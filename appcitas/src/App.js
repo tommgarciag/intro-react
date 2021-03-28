f83c2c184cb90e41fab22d869fa52ab44b8524f0
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2021-03-28 19:31:58 -0300

    Agrego citas al local storage

--- appcitas/src/App.js
+++ appcitas/src/App.js
@@ -1,11 +1,27 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 function App() {
 
+
+  // Citas en local storage
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales) {
+    citasIniciales = [];
+  }
+
   // State principal (arreglo de citas)
-  const [citas, guardarCitas] = useState([]);
+  const [citas, guardarCitas] = useState(citasIniciales);
+
+  // Use Effect para realizar ciertas operaciones cuando el state cambie
+  useEffect( () => {
+    if(citasIniciales) {
+      localStorage.setItem('citas', JSON.stringify(citas));
+    } else {
+      localStorage.setItem('citas', JSON.stringify([]));
+    }
+  }, [citas]);
 
   // Funcion que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
@@ -21,6 +37,9 @@ function App() {
     guardarCitas(nuevasCitas);
   }
 
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? "No hay citas" : "Administra tus citas";
+
 
   return (
     <Fragment>
@@ -33,7 +52,7 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map(cita => (
               <Cita
                 key={cita.id}
