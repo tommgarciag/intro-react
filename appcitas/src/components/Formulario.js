f4e609c54741c7fb74bb998345d4593cc8ed3d3b
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2021-03-28 19:42:25 -0300

    Documento mis componentes con PropTypes

--- appcitas/src/components/Formulario.js
+++ appcitas/src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
 
 const Formulario = ({crearCita}) => {
 
@@ -113,6 +114,11 @@ const Formulario = ({crearCita}) => {
             </form>
         </Fragment>
      );
+}
+
+// Documentacion de componente
+Formulario.propTypes = {
+    crearCita: PropTypes.func.isRequired
 }
  
 export default Formulario;
