b5dfddc67ed2c925aac8292d2f4a4fcdcf89806a
Tomas Ignacio Garcia <15tommgarcia@gmail.com>
2021-04-07 20:57:31 -0300

    Lee los el presupuesto ingresado, valido y programo mensaje de error en caso de que el presupuesto no sea valido

---
+++ presupuestos/src/components/Errores.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Errores = ({mensaje}) => (
+     <p className="alert alert-danger error">{mensaje}</p>
+     
+);
+
+ 
+export default Errores;
--- presupuestos/src/components/Pregunta.js
+++ presupuestos/src/components/Pregunta.js
@@ -1,15 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Errores from './Errores';
 
 const Pregunta = () => {
+
+    // Definir state
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    // Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    // Submit para definir presupuesto
+
+    const agregarPresupusto = e => {
+        e.preventDefault();
+
+        // Validar
+        if(cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true);
+            return;
+        }
+
+        // Si pasa la validacion
+        guardarError(false);
+        
+    }
+
+
     return ( 
         <Fragment>
             <h2>Coloca tu presupuesto</h2>
 
-            <form>
+            {error ? <Errores mensaje="El presupuesto es Incorrecto" /> : null}
+
+            <form 
+                onSubmit={agregarPresupusto}
+            >
+
                 <input 
                     type="number"
                     className="u-full-width"
                     placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
                 
                 />
 
